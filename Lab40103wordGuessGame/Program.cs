bfa3a2998d0e06f75bf1f41ac1d988dcb69ea5bb
Andrew Hinojosa <drewsview34@gmail.com>
2019-01-13 19:29:56 -0800

    Implemented Code for Main method

--- Lab40103wordGuessGame/Program.cs
+++ Lab40103wordGuessGame/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.IO;
 
 namespace Lab40103wordGuessGame
 {
     class Program
-    {
+    { /// <summary>
+    /// Calls the interface and creates file to interact with words in the game
+    /// </summary>
+    /// <param name="args"></param>
         static void Main(string[] args)
         {
+            string location = "../../../words.txt";
             Console.WriteLine("Hello World!");
+            Createfile(location);
+            UserInterface(location);
         }
     }
 }
